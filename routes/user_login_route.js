dae1206f45104ad5adfd149ea42f10f3771ebcb3
Thrive Coder <thrivetech793@gmail.com>
2023-08-02 12:08:38 -0700

    praying right now

--- routes/user_login_route.js
+++ routes/user_login_route.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const UserLogin = require("../Model/user_login_model")
+const userDetails = require("../Model/user_login_model")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
   const { email, password } = req.body
 
   try {
-    const user = await UserLogin.findOne({ email })
+    const user = await userDetails.findOne({ email })
 
     if (!user) {
       return res.status(404).json({ message: "User not found" })
